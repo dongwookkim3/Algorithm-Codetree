4f4a1b23302b1fbfee12088005851dedc844e9a7
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-10 00:07:50 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 130ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 231210/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int k, n;
+vector<int> selected_nums;
+void PrintPermutation(){
+    for(int i=0;i<(int)selected_nums.size();i++)
+        cout << selected_nums[i] << " ";
+    cout << endl;
+}
+void FindPermutations(int cnt){
+    if (cnt==n){
+        PrintPermutation();
+        return;
+    }
+    for (int i=1;i<=k;i++){
+        selected_nums.push_back(i);
+        FindPermutations(cnt+1);
+        selected_nums.pop_back();
+    }
+}
+int main(){
+    cin >> k >> n;
+    FindPermutations(0);
+    return 0;
+}
