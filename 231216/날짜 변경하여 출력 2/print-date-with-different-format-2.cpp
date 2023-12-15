1add9c70dd56916f92172e8418dbaf0a4089b43d
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-16 00:54:07 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231216/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int y,m,d;
+    cin >> d;
+    cin.get();
+    cin >> m;
+    cin.get();
+    cin >> y;
+    cout << y << '.' << d << '.' << m;
+    return 0;
+}
