168c5ba7a9e94c5ab58a89bd5915532c1cabd3e7
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-15 11:49:21 +0900

    Title: 자연수면 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231215/자연수면 출력/output-only-natural-number.cpp
+++ 231215/자연수면 출력/output-only-natural-number.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main(){
     int a,b;
     cin >> a >> b;
+    if (a<=0){
+        cout << 0;
+        return 0;
+    }
     while(b--){
-        if (a<=0) cout << 0;
         else cout << a;
     }
     return 0;
