234043863a21ea6d2a5b7e5660aed6b40e57f267
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-15 12:06:51 +0900

    Title: 총합 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231215/총합 구하기/find-the-total.cpp
+++ 231215/총합 구하기/find-the-total.cpp
@@ -4,7 +4,9 @@ int main(){
     int a,b,m=0;
     cin >> a >> b;
     for (int i=a;i<=b;i++){
-        if (a%6==0 && a%8!=0) m+=i;
+        if (a%6==0 && a%8!=0){
+            m+=i;
+        }
     }
     cout << m;
     return 0;
