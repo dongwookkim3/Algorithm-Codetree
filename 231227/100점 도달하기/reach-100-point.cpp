9341d526a8bbaa3b8c35346d34db27bc6a965d5e
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-27 00:27:54 +0900

    Title: 100점 도달하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231227/100점 도달하기/reach-100-point.cpp
+++ 231227/100점 도달하기/reach-100-point.cpp
@@ -8,7 +8,7 @@ int main(){
         else if (i>=80) cout << "B ";
         else if (i>=70) cout << "C ";
         else if (i>=60) cout << "D ";
-        else cout << "D ";
+        else cout << "F ";
     }
     return 0;
 }
