59ba9256d1de9f994c715dd25eb7639a22a17745
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-27 00:23:49 +0900

    Title: 자연수면 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if (a<=0){
+        cout << 0;
+        return 0;
+    }
+    while(b--){
+        cout << a;
+    }
+    return 0;
+}
