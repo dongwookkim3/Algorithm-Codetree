b5dd0956b79ceddf003208d7a1d1c2b8c385bd9b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-30 21:19:17 +0900

    Title: 특정 조건 두 정수 비교, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231230/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if (a<b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+    if (a==b) cout << 1;
+    else cout << 0;
+    return 0;
+}
