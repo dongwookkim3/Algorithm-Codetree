c460adb129adf31776c401e53a22794f5691aefc
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-30 21:45:49 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/굉장한 숫자/amazing-number.cpp
+++ 231230/굉장한 숫자/amazing-number.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    cout << ((n%2==1 && n%3==0) || (n%2==0 && n%5==0));
+    if ((n%2==1 && n%3==0) || (n%2==0 && n%5==0)) cout << "true";
+    else cout << "false";
     return 0;
 }
