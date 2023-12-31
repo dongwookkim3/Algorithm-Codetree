d2d2d4635a76ff2a1ed12e505138df70469e596f
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-31 23:13:19 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231231/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m=0;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        if (i%4==0){
+            if (i%4!=0){
+                m++;
+            }
+            else if (i%400==0){
+                m++;
+            }
+            else m++;
+        }
+    }
+    cout << m;
+    return 0;
+}
