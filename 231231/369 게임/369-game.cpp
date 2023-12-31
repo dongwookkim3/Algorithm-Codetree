ebad1e16d6fc02a13166ae7dc7eab9f6bc977994
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-31 23:03:48 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231231/369 게임/369-game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+bool f(int n){
+    if (n%10==3 || n%10==6 || n%10==9) return 1;
+    else if (n>9) return f(n/10);
+    else return 0;
+}
+int main(){
+    int n;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        if f(i) cout << "0 ";
+        else cout << i << ' ';
+    }
+    return 0;
+}
