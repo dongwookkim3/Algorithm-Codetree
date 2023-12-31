bc3a96221c4da460cb42f33f0ed2d62e82e35b06
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2023-12-31 23:10:41 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231231/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m1=0,m2=0,m3=0;
+    for (int i=1;i<=n;i++){
+        if (i%12==0) m3++;
+        else if (i%6==0) m2++;
+        else if (i%3==0) m2++;
+        else if (i%2==0) m1++;
+    }
+    cout << m1 << ' ' << m2 << ' ' << m3;
+    return 0;
+}
