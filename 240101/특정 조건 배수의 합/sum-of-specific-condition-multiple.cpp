c3d048d07eb021b3b240cbfd8a0e2b7700222e94
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-01 23:06:08 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240101/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
     int a,b,m=0;
     cin >> a >> b;
-    for (int i=a;i<=b;i++){
+    for (int i=min(a,b);i<=max(a,b);i++){
         if (i%5==0){
             m+=i;
         }
