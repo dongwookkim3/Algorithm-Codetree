213ae67648c5f109799a4455a4e0b439f88dfd7c
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-01 23:09:14 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/범위 안의 수 2/number-in-range-2.cpp
+++ 240101/범위 안의 수 2/number-in-range-2.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    float m=0;
+    int m=0,m1=0;
     for (int i=1;i<=10;i++){
         int a;
         cin >> a;
         if (a>=0 && a<=200) {
             m+=a;
+            m1++;
         }
     }
     cout << fixed;
     cout.precision(1);
-    cout << m << ' ' << m/10;
+    cout << m << ' ' << (float)m/m1;
     return 0;
 }
