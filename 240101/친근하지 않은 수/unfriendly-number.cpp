a4c836b8621e1435d89d50a08dfc005556973d28
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-01 23:18:02 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240101/친근하지 않은 수/unfriendly-number.cpp
+++ 240101/친근하지 않은 수/unfriendly-number.cpp
@@ -4,7 +4,7 @@ int main(){
     int n,m=0;
     cin >> n;
     for (int i=1;i<=n;i++){
-        if (i%2==0 && i%3==0 && i%5==0) continue;
+        if (i%2!=0 && i%3!=0 && i%5!=0) continue;
         m++;
     }
     cout << m;
