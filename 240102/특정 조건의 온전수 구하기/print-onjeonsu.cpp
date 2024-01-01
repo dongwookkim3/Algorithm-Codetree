b37802c7c4829c01c9cb8cead7effe6144423793
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-02 07:00:59 +0900

    Title: 특정 조건의 온전수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/특정 조건의 온전수 구하기/print-onjeonsu.cpp
+++ 240102/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -4,8 +4,10 @@ int main(){
     int n;
     cin >> n;
     for (int i=1;i<=n;i++){
-        if (i%2 && i%10!=5 && (i%3!=0 && i%9==0)){
-            cout << i << ' ';
+        if (i%2 && i%10!=5){
+            if (i%3!=0 || (i%3==0 && i%9==0)){
+                cout << i << ' ';
+            }
         }
     }
     return 0;
