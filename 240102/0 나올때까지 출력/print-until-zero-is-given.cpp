fde721735b1ea211ba2d43e6891f98520e398aef
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-02 09:55:42 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    while(1){
+        int a;
+        cin >> a;
+        if (a==0) break;
+        cout << a << "\n";
+    }
+    return 0;
+}
