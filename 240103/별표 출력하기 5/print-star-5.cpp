4e81eb3a375e144678e2bbfac66d850edfc692f1
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-03 08:59:39 +0900

    Title: 별표 출력하기 5, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240103/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for (int i=n;i>=1;i--){
+        for (int j=i;j>=1;j--){
+            for (int l=1;k<=j;k++){
+                cout << '*';
+            }
+            cout << ' ';
+        }
+        cout << "\n";
+    }
+    return 0;
+}
