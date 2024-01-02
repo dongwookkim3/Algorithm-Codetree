c5b8bac0d392310ce7012916d3b83af4a48d6b03
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-02 09:49:24 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 240102/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -4,8 +4,14 @@ int main(){
     int a;
     cin >> a;
     for (int i=1;i<=a;i++){
-       if ((i%2==0 && i%4!=0) || ((i%8)%2==0) || (i%7<4)) continue;
-       cout << i << ' ';
+        if (i%2==0){
+            if (i%4!=0){
+                continue;
+            } 
+        }
+        if ((i/8)%2==0) continue;
+        if ((i%7)<4) continue;
+        cout << i << ' ';
     }
     return 0;
 }
