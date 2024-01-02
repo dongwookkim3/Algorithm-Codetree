3c03b4ebe8f2374b7f685b6f4c83b764e2e67bcc
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-02 13:01:39 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240102/몫 출력하기/print-share.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    while(cin >> n){
+        if (cin.eof) break;
+        if (i%2==0){
+            cout << i/2 << "\n";
+        }
+    }
+    return 0;
+}
