6bf89adbd91a90481702503bbf84529db7e431b8
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-02 14:16:34 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/3n + 1 수열/3n-plus-1-sequence.cpp
+++ 240102/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for (int i=1;;i++){
-        if (n%2==0) n/=2;
-        else n=n*3+1;
+    for (int i=0;;i++){
         if (n==1){
             cout << i;
             break;
         }
+        if (n%2==0) n/=2;
+        else n=n*3+1;
     }
     return 0;
 }
