302c751c5b31cb3465f729e1d8a9141c0ad3dec6
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-02 13:04:18 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/몫 출력하기/print-share.cpp
+++ 240102/몫 출력하기/print-share.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    while(1){
+    int m=3;
+    while(m){
+        int n;
         cin >> n;
         if (n%2==0){
+            m--;
             cout << n/2 << "\n";
         }
     }
