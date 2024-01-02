ed8ca962cc8738371b7ddeb582d343a62d8c5457
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-03 08:47:20 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for (int i=2;i<n;i++){
+        if (n%i==0){
+            cout << 'C';
+            return 0;
+        }
+    }
+    cout << 'N';
+    return 0;
+}
