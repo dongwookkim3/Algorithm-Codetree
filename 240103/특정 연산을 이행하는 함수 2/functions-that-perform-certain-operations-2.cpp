8297eb70347def27c0fde06087eb4c400aea0dd8
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-03 13:37:57 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240103/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.cpp
+++ 240103/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.cpp
@@ -5,15 +5,6 @@ using namespace std;
 int main(){
     float a,b,c;
     cin >> a >> b >> c;
-    if (a==max(a,max(b,c))) cout << ceil(a) << ' ';
-    else if (a==min(a,min(b,c))) cout << floor(a) << ' ';
-    else cout << round(a) << ' ';
-    if (b==max(a,max(b,c))) cout << ceil(b) << ' ';
-    else if (b==min(a,min(b,c))) cout << floor(b) << ' ';
-    else cout << round(b) << ' ';
-    if (c==max(a,max(b,c))) cout << ceil(c);
-    else if (c==min(a,min(b,c))) cout << floor(c);
-    else cout << round(c);
-    
+    cout << ceil(max(a,max(b,c))) << ' ' << floor(min(a,min(b,c))) << ' ' << round(a+b+c-max(a,max(b,c))-min(a,min(b,c)));
     return 0;
 }
