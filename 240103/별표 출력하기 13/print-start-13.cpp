48761f058dd42438b12639a86f0e2262a562df04
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-03 22:08:24 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        if (i%2){
+            for (int j=1;j<=n-i/2;j++){
+                cout << "* ";
+            }
+        }
+        else {
+            for (int j=1;j<=i/2;j++){
+                cout << "* ";
+            }
+        }
+        cout << "\n";
+    }
+    for (int i=n;i>=1;i--){
+        if (i%2){
+            for (int j=1;j<=n-i/2;j++){
+                cout << "* ";
+            }
+        }
+        else {
+            for (int j=1;j<=i/2;j++){
+                cout << "* ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
