0a565e0fb357f6d8ea3d2c3aca88dd1bfb11b6fd
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-04 21:31:57 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=0;
+    for (int i=1;i<=10;i++){
+        int a;
+        cin >> a;
+        n+=a;
+    }
+    cout << n;
+    return 0;
+}
