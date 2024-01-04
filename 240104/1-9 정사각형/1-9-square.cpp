50ba15397b4c0b7bcb8997a1d73de1bdaaa79c78
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-04 21:30:44 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240104/1-9 정사각형/1-9-square.cpp
+++ 240104/1-9 정사각형/1-9-square.cpp
@@ -6,7 +6,7 @@ int main(){
     for (int i=1;i<=n;i++){
         for (int j=1;j<=n;j++){
             int m=(i-1)*n+j;
-            if (m/10) m++;
+            m+=m/10;
             cout << m%10;
         }
         cout << "\n";
