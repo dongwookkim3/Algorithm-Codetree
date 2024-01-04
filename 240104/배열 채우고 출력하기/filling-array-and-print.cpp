ac5ffb22deefef9c730b535b2547fcd51b1d9d4a
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-04 21:35:56 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240104/배열 채우고 출력하기/filling-array-and-print.cpp
+++ 240104/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     string A;
     getline(cin,A);
-    for (int i=A.length-1;i>=0;i--){
+    for (int i=18;i>=0;i--){
         if (A[i]!=' ') cout << A[i];
     }
     return 0;
