08434f42fba85cd7bf00ee8a5df97ebe9e78ae6e
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-05 10:30:59 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/행과 열/row-column.cpp
+++ 240105/행과 열/row-column.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cin >> n;
+    int n,m;
+    cin >> n >> m;
     for (int i=1;i<=n;i++){
-        for (int j=1;j<=n;j++){
+        for (int j=1;j<=m;j++){
             cout << i*j << ' ';
         }
         cout << "\n";
