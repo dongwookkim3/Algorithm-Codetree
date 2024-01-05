b8453812fddd95f5fb73632fc0b580c1b410bff1
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-05 10:29:51 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
+++ 240105/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for (int i=1li<=n;i++){
+    for (int i=1;i<=n;i++){
         for (int j=n;j>=1;j--){
-            cout << j;
+            cout << j << ' ';
         }
         cout << "\n";
     }
