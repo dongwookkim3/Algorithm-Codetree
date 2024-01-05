88f4ae6a6a9850b6243cbb62613d95a41b9885c4
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-05 10:13:59 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/별표 출력하기 12/print-star-12.cpp
+++ 240105/별표 출력하기 12/print-star-12.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
+    if (n==1) cout << '*';
     for (int i=1;i<=(n/2)*2;i++){
         for (int j=1;j<=n;j++){
             if (i==1 || (j%2==0 && j>=i)){
