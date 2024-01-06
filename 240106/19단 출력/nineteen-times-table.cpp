d146dde2eb9a24f2bf09473b98ac90076efb7b7a
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-06 23:23:41 +0900

    Title: 19단 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/19단 출력/nineteen-times-table.cpp
+++ 240106/19단 출력/nineteen-times-table.cpp
@@ -4,7 +4,7 @@ int main(){
     for (int i=1;i<=19;i++){
         for (int j=1;j<=19;j++){
             cout << i << " * " << j << " = " << i*j;
-            if (j%2==1) cout << " / ";
+            if (j%2==1 && j!=19) cout << " / ";
             else cout << "\n";
         }
     }
