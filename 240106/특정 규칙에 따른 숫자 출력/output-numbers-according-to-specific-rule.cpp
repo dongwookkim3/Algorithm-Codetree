31f522ed6c48285b824ecff8bfcdc6d4a4469fb1
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-06 23:11:40 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m=1;
+    cin >> n;
+    for (int i=n;i>=1;i--){
+        for (int j=1;j<=n-i;j++){
+            cout << "  ";
+        }
+        for (int j=1;j<=i;j++){
+            cout << m;
+            m++;
+            if (m==10) m=1;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
