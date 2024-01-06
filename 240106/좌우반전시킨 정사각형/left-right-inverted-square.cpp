c9aa25ea4e68ea56b0ccb8b9a6dd5fe668040ba6
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-06 11:25:51 +0900

    Title: 좌우반전시킨 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/좌우반전시킨 정사각형/left-right-inverted-square.cpp
+++ 240106/좌우반전시킨 정사각형/left-right-inverted-square.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
 	int n;
     cin >> n;
-    for (int i=n;i>=1;i--){
-        for (int j=1;j<=n;j++){
+    for (int i=1;i<=n;i++){
+        for (int j=n;j>=1;j--){
             cout << i*j << ' ';
         }
         cout << "\n";
