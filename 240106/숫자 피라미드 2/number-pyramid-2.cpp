7eb517fe59e4a3dab995c5a614a5459cad8965c6
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-06 22:59:36 +0900

    Title: 숫자 피라미드 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m=1;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            cout << m;
+            m++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
