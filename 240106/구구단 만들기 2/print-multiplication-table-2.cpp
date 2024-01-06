8de51f93f5338d1d611c682d3d833dbf9653d87e
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-06 11:53:18 +0900

    Title: 구구단 만들기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/구구단 만들기 2/print-multiplication-table-2.cpp
+++ 240106/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a,b;
     cin >> a >> b;
-    for (int i=1;i<=9;i++){
+    for (int i=2;i<=8;i+=2){
         for (int j=b;j>=a;j--){
             cout << j << " * " << i << " = " << i*j;
             if (j==a) cout << "\n";
