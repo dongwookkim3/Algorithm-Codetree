80dc21e55f71fa3ffbbdf640982b8fb73deb85aa
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-07 23:23:39 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240107/배열 채우기/filling-array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int A[10]={};
+    for (int i=0;;i++){
+        cin >> A[i];
+        if (A[i]==0){
+            for (int j=i-1;j>=0;j--){
+                cout << A[j] << ' ';
+            }
+            return 0;
+        }
+    }
+    return 0;
+}
