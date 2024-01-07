e67d5ae99fa1a37944b0bb51da9842f9e13a0594
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-07 22:53:58 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/완전수/perfect-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,n=0;
+    cin >> a >> b;
+    for (int i=a;i<=b;i++){
+        int m=0;
+        for (int j=1;j<i;j++){
+            if (i%j==0) m+=j;
+        }
+        if (i==m) n++;
+    }
+    cout << n;
+    return 0;
+}
