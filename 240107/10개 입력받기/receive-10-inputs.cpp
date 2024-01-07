28b1b233a7beff7915e170cb351665911af23508
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-07 23:27:53 +0900

    Title: 10개 입력받기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240107/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float m=0;
+    for (i=1;i<=10;i++){
+        int a;
+        cin >> a;
+        m+=a;
+        if (a==0){
+            i--;
+            break;
+        }
+    }
+    cout << m << ' ';
+    cout << fixed;
+    cout << precision(1);
+    cout << m/i;
+    return 0;
+}
