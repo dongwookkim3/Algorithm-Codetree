45054b43b47bfd69c0688252b4510ba5f7ee27c3
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-07 23:02:10 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240107/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=0;
+    float m=0;
+    for (int i=1;i<=10;i++){
+        int a;
+        cin >> a;
+        if (a>250){
+            cout << m << ' ' << m/n;
+            return 0;
+        }
+        m+=a;
+        n++;
+    }
+    cout << m << ' ' << m/n;
+    return 0;
+}
