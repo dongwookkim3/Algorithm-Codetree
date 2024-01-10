b72f57a07194adf4d5666d958a38f4ceb018e811
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-10 17:26:48 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1000
+using namespace std;
+int main(){
+    int n;
+    long long A[MAX+1]={};
+    long long B[MAX+1]={};
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        cin >> A[i];
+    }
+    for (int i=2;i<=n;i++){
+        int m=1000;
+        for (int j=1;j<=i;j++){
+            if (A[j]<m){
+                m=A[j];
+            }
+        }
+        B[i]=A[i]-m;
+    }
+    sort(B+1,B+n+1,greater<int>());
+    cout << B[1];
+    return 0;
+}
