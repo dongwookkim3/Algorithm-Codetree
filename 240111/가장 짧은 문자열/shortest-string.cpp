c6ff1b48c49e9463043bfe7744ec0a1f8959a85b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-11 21:36:35 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/가장 짧은 문자열/shortest-string.cpp
+++ 240111/가장 짧은 문자열/shortest-string.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int m=20;
+    int max=1,min=20;
     for (int i=1;i<=3;i++){
         string A;
         cin >> A;
-        if (m>A.length()){
-            m=A.length();
+        if (max<A.length()){
+            max=A.length();
+        }
+        if (min>A.length()){
+            min=A.length();
         }
     }
-    cout << m;
+    cout << max-min;
     return 0;
 }
