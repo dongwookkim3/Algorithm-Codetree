3788b4a197b486974cce64a8aa833e73a2b7d750
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-12 18:15:20 +0900

    Title: 공백을 기준으로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240112/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i=1;i<=2;i++){
+        string A;
+        getline(cin,A);
+        for (int j=0;j<a.length();j++){
+            if (A[j]!=' '){
+                cout << A[i];
+            }
+        }
+    }
+    return 0;
+}
