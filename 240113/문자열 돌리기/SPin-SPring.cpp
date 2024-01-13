a656899547735667a2dd633e1beecd0287cf4ff0
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-13 22:05:50 +0900

    Title: 문자열 돌리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/문자열 돌리기/SPin-SPring.cpp
+++ 240113/문자열 돌리기/SPin-SPring.cpp
@@ -4,12 +4,10 @@ int main(){
     string A;
     cin >> A;
     string B=A;
-    cout << B << "\n";
-    B=B[B.length()-1]+B.substr(0,B.length()-1);
-    while(A!=B){
+    do{
         cout << B << "\n";
         B=B[B.length()-1]+B.substr(0,B.length()-1);
-    }
+    }while(A!=B);
     cout << B;
     return 0;
 }
