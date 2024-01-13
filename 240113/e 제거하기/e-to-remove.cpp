48635a24bf3da43c0e5160a66da1748b79b6c262
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-13 21:55:31 +0900

    Title: e 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240113/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string A;
+    cin >> A;
+    for (int i=0;i<A.length();i++){
+        if (A[i]=='e'){
+            A.erase(i,1);
+            break;
+        }
+    }
+    cout << A;
+    return 0;
+}
