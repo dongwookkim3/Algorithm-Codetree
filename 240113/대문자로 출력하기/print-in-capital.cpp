c438925b80f85b41899aa2e2d0b901f80b6ea5bf
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-13 22:45:41 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/대문자로 출력하기/print-in-capital.cpp
+++ 240113/대문자로 출력하기/print-in-capital.cpp
@@ -5,7 +5,7 @@ int main(){
     cin >> A;
     for (int i=0;i<=A.length();i++){
         if (A[i]>=65 && A[i]<=90) cout << A[i];
-        else if (A[i]>=96 && A[i]<=123) cout << (char)(A[i]-32);
+        else if (A[i]>=97 && A[i]<=122) cout << (char)(A[i]-32);
     }
     return 0;
 }
