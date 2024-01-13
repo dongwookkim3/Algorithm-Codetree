255544f710ed20e032056a5cb00c86c4e3d1f72b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-13 22:35:18 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
+++ 240113/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(){
     char a,b;
     cin >> a >> b;
-    cout << (int)a+b << ' ' << (int)a-b;
+    cout << (int)a+b << ' ' << abs((int)a-b);
     return 0;
 }
