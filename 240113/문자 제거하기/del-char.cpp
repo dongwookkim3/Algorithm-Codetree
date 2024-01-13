05d2b8b21f24c72546fb7f2dc4cabeb37daf30fa
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-13 21:12:11 +0900

    Title: 문자 제거하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/문자 제거하기/del-char.cpp
+++ 240113/문자 제거하기/del-char.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main(){
     string A;
-    int n;
-    cin >> A >> n;
-    while(n--){
+    cin >> A;
+    while(A.length()>1){
         int a;
         cin >> a;
-        A.erase(a,1);
+        if (a>A.length()-1) A.erase(A.length()-1,1);
+        else A.erase(a,1);
         cout << A << "\n";
     }
     return 0;
