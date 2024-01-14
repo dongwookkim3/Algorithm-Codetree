edda48e1cf8e8d7c1b20f27f6bb4ec17dda7580a
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-14 16:09:17 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240114/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void Print(int n){
+    for (int i=1;i<=n;i++){
+        cout << "12345^&*()_\n";
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    Print(n);
+    return 0;
+}
