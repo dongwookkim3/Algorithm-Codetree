fdcea16cb89332a7a616603731bf07ac45d24e86
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-15 23:11:36 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240115/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+bool Prime(int n){
+    for (int i=2;i<n;i++){
+        if (n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+int f(int n){
+    if (n>9) return f(n/10)+n%10;
+    else reutrn n;
+}
+int main(){
+    int a,b,m=0;
+    cin >> a >> b;
+    for (int i=a;i<=b;i++){
+        if (Prime(i) && !(f(i)%2)){
+            m++;
+        }
+    }
+    cout << m;
+    return 0;
+}
