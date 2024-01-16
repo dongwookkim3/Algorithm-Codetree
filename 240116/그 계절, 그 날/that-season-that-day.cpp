b51cc2be19ae69b6e242b0e96dc05170a0917f25
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-16 16:41:44 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/그 계절, 그 날/that-season-that-day.cpp
+++ 240116/그 계절, 그 날/that-season-that-day.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int g(int y){
+bool g(int y){
     if (y%4==0){
         if (y%100==0){
             if (y%400==0){
@@ -14,17 +14,18 @@ int g(int y){
 }
 bool f(int y,int m,int d){
     if (m==4 || m==6 || m==9 || m==11){
-        if (d>30){
+        if (d==31){
             return 0;
         }
         return 1;
     }
-    else if (m==2){
+    if (m==2){
         if (d>28+g(y)){
             return 0;
         }
         return 1;
     }
+    return 1;
 }
 int main(){
     int y,m,d;
