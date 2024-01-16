6eac06f62924922e5a277ba167e6b6e05e6bfa16
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-16 16:38:50 +0900

    Title: 그 계절, 그 날, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int g(int y){
+    if (y%4==0){
+        if (y%100==0){
+            if (y%400==0){
+                return 1;
+            }
+            return 0;
+        }
+        return 1;
+    }
+    return 0;
+}
+bool f(int y,int m,int d){
+    if (m==4 || m==6 || m==9 || m==11){
+        if (d>30){
+            return 0;
+        }
+        return 1;
+    }
+    else if (m==2){
+        if (d>28+g(y)){
+            return 0;
+        }
+        return 1;
+    }
+}
+int main(){
+    int y,m,d;
+    cin >> y >> m >> d;
+    if (!f(y,m,d)) cout << -1;
+    else if (3<=m && m<=5) cout << "Spring";
+    else if (6<=m && m<=8) cout << "Summer";
+    else if (9<=m && m<=11) cout << "Fall";
+    else cout << "Winter";
+    return 0;
+}
