2fedf6d4cc3f761e90bcdc823b063d47dfe7137a
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-18 21:58:19 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240118/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int f(int n){
+    if (n==1) return 0;
+    else if (n%2==0) return f(n/2)+1;
+    else return f(3*n+1)+1;
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << f(n);
+    return 0;
+}
