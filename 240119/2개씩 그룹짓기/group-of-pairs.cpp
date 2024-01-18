4da72defcb3f95b44a968593746d3ac94c9a95d0
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-19 02:42:46 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240119/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240119/2개씩 그룹짓기/group-of-pairs.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <algorithm>
+#define MAX 2001
 using namespace std;
 int main(){
     int n,m=0;
+    int A[MAX]={};
     cin >> n;
-    while(n--){
-        int a,b;;
-        cin >> a >> b;
-        if (a+b>m){
-            m=a+b;
+    for (int i=0;i<2*n;i++){
+        cin >> A[i];
+    }
+    sort(A,A+2*n+1);
+    A[0]=A[2*n];
+    for (int i=0;i<2*n;i++){
+        if (A[i+1]+A[i]>m && i%2==0){
+            m=A[i+1]+A[i];
         }
     }
     cout << m;
