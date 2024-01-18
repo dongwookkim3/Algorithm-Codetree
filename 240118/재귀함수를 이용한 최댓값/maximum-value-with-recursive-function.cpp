cb3392276ac5a40723a677b9d9020884b9e14f7e
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-01-18 21:49:55 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int f(int n,int m){
+    int a;
+    cin >> a;
+    if (a>m){
+        m=a;
+    }
+    if (n==0) return m;
+    else return f(n-1,m);
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << f(n,0);
+    return 0;
+}
