4503153b52f0f5b7cc600c07eee5134d7c4f25b4
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-06 06:30:22 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240206/Date to Date/date-to-date.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int M[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    int m1,d1,m2,d2,D=0;
+    cin >> m1 >> d1 >> m2 >> d2;
+    while(m1!=m2 || d1!=d2){
+        D++;
+        d1++;
+        if (M[m1-1]==d1){
+            m1++;
+            d1=0;
+            if (m1==13){
+                m1=1;
+            }
+        }
+    }
+    cout << D;
+    return 0;
+}
