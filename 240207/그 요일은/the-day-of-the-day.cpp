14c46c62b15c300cfe9b137c6a047e1bb9a9dde7
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-07 23:43:11 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240207/그 요일은/the-day-of-the-day.cpp
+++ 240207/그 요일은/the-day-of-the-day.cpp
@@ -3,12 +3,12 @@
 #define MAX 12
 using namespace std;
 int main(){
-    int M[MAX+1]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int M[MAX+1]={0,31,29,31,30,31,30,31,31,30,31,30,31};
     string DAY[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
     for (int i=1;i<=MAX;i++){
         M[i]+=M[i-1];
     }
-    int m1,m2,d1,d2,D=1,m=0;
+    int m1,m2,d1,d2,D=0,m=0;
     string A;
     cin >> m1 >> d1 >> m2 >> d2 >> A;
     for (int i=0;i<7;i++){
