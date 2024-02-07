4dbff6a513c1f32d7c84b1c8d4f03ed5fd0cd0a7
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-07 23:24:26 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240207/그 요일은/the-day-of-the-day.cpp
+++ 240207/그 요일은/the-day-of-the-day.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 #define MAX 12
 using namespace std;
 int main(){
     int M[MAX+1]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-    vector<string> DAY={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    string DAY[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
     for (int i=1;i<=MAX;i++){
         M[i]+=M[i-1];
     }
-    int m1,m2,d1,d2;
+    int m1,m2,d1,d2,D=0,m=0;
     string A;
     cin >> m1 >> d1 >> m2 >> d2 >> A;
-    int D=(M[m2-1]+d2-M[m1-1]+d1)/7;
     for (int i=0;i<7;i++){
         if (DAY[i]==A){
-            if ((M[m2-1]+d2-M[m1-1]+d1)%7>=i){
-                D++;
-            }
-            break;
+            m=i;
         }
     }
-    cout << D-1;
+    for (int i=M[m1-1]+d1;i<=M[m2-1]+d2;i++){
+        if (i%7==m){
+            D++;
+        }
+    }
+    cout << D;
     return 0;
 }
