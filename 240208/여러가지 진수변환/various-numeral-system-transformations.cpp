cbfe448e75a6026070299359c20fff50ed5f399d
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-08 23:17:20 +0900

    Title: 여러가지 진수변환, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240208/여러가지 진수변환/various-numeral-system-transformations.cpp
+++ 240208/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 int main(){
     string A;
     int a,b;
     cin >> a >> b;
-    for (int i=pow(b,6);i>=0;i/=b){
+    for (int i=pow(b,5);i>0;i/=b){
         if (a/i){
-            A+=a/i;
+            A+=to_string(a/i);
             a%=i;
         }
     }
