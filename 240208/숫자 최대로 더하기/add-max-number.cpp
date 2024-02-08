af4c4d1b1fbc6fdf784495f3fd76554cebf7613d
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-08 22:14:09 +0900

    Title: 숫자 최대로 더하기, Time: 55ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240208/숫자 최대로 더하기/add-max-number.cpp
+++ 240208/숫자 최대로 더하기/add-max-number.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 int main(){
-    vector<float> A;
+    vector<long long> A;
     int n;
     cin >> n;
     while(n--){
@@ -12,12 +12,13 @@ int main(){
         A.push_back(a);
     }
     sort(A.begin(),A.end(),greater<int>());
-    while(A.size()!=1){
-        A.front()+=A.back()/2;
+    while(A.size()!=2){
+        long long m=A.back();
         A.pop_back();
+        A.back()+=m;
     }
     cout << fixed;
     cout.precision(1);
-    cout << A.front();
+    cout << float(A.front()+A.back()/2);
     return 0;
 }
