f076891ae4a137028f24ecb7ede977172f88373b
Dongwook_Kim <128461452+dongwookkim3@users.noreply.github.com>
2024-02-08 21:56:20 +0900

    Title: 수의 곱셈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240208/수의 곱셈/multiplication-of-numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int m=1;
+    vector<int> A,B;
+    for (int i=1;i<=3;i++){
+        int a;
+        cin >> a;
+        if (a%2==1){
+            A.push_back(a);
+        }
+        else {
+            B.push_back(a);
+        }
+    }
+    if (B.size()%2==1){
+        for (int i=0;i<B.size();i++){
+            m*=B[i];
+        }
+    }
+    else {
+        for (int i=0;i<A.size();i++){
+            m*=A[i];
+        }
+        for (int i=0;i<B.size();i++){
+            m*=B[i];
+        }
+    }
+    cout << m;
+    return 0;
+}
